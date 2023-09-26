443d668d073c1bdd33611e61a9c7b72f47575080
HoneyBunny <paul.messeant@epita.fr>
2023-09-27 00:56:36 +0200

    udpate clamp on borders

--- Pong/Scripts/Brick.gd
+++ Pong/Scripts/Brick.gd
@@ -2,15 +2,18 @@ extends CharacterBody2D
 
 @export var speed = 800
 @export var player = 0
+var inputs = ["ui_left", "ui_right"]
+var screen_size = 0
 
-func _process(delta):
-	var inputs = ["ui_left", "ui_right"]
+func _init():
 	if player != 0:
 		inputs = ["key_left", "key_right"]
-	
-	if Input.is_action_pressed(inputs[0]):
+		
+func _ready():
+	screen_size = get_viewport().get_visible_rect().size
+		
+func _process(delta):
+	if Input.is_action_pressed(inputs[0]) and position.x > 0:
 		position.x -= delta * speed
-	if Input.is_action_pressed(inputs[1]):
+	if Input.is_action_pressed(inputs[1]) and position.x < screen_size.y:
 		position.x += delta * speed
-
-	
